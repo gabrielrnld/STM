470a465fe742028d39526de0efd8d2f0c402ca64
Rifqi Fauzan <rifqifauzan2096@gmail.com>
2022-09-28 13:31:11 +0700

    adjust login

--- src/components/layout/Headers.js
+++ src/components/layout/Headers.js
@@ -1,40 +1,45 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Headers() {
-  const token = localStorage.getItem("authorization");
+  const token = localStorage.getItem('authorization');
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("authorization");
-    window.location.reload();
+    localStorage.removeItem('authorization');
+    // window.location.reload();
+    navigate('/');
   };
 
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" sticky="top">
         <Container>
           <Navbar.Brand as={Link} to="home" className="title-fs fw-bold">
             STM Apartment
           </Navbar.Brand>
           <Navbar.Collapse className="justify-content-end">
-            <Nav>
-              <Nav.Link as={Link} to="home">
-                Home
-              </Nav.Link>
-              <Nav.Link>Price</Nav.Link>
-              <Nav.Link>About</Nav.Link>
-              {token && token !== "undefined" ? (
-                <Nav.Link as="button" onClick={handleLogout}>
-                  Logout
+            {token && token !== 'undefined' ? (
+              <Nav>
+                <Nav.Link as={Link} to="transaksi">
+                  Transaction
                 </Nav.Link>
-              ) : (
+                <Nav.Link as={Link} to="list-apartments">
+                  List Apartment
+                </Nav.Link>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </Nav>
+            ) : (
+              <Nav>
+                <Nav.Link as={Link} to="home">
+                  Home
+                </Nav.Link>
                 <Nav.Link as={Link} to="login">
                   Login
                 </Nav.Link>
-              )}
-            </Nav>
+              </Nav>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
